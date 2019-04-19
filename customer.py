b03032b4bf24f663229b2b7abeb56dcef5014236
Yoshiki <villager2525@gmail.com>
2019-04-19 14:43:05 +0900

    他の方の考え方を追加｡ コメントを追加｡

--- customer.py
+++ customer.py
@@ -10,18 +10,25 @@ class Customer:
     def info_csv(self):
         return f"{self.full_name()},{self.age}"
 
+    # 自分の考え
     def display_profile(self):
         return f"Name:{self.full_name()} Age:{self.age}"
+
+    def display_profile_2(self):
+        print(f"Name:{self.full_name()} Age:{self.age}")
 
 
 if __name__ == "__main__":
     # "Tom Ford"
     tom = Customer("Tom", "Ford", 57)
     print(tom.full_name())
-    print(tom.display_profile())  # "Name: Tom Ford, Age: 57"
+    print(tom.display_profile())  # 自分の考え # "Name: Tom Ford, Age: 57"
+    tom.display_profile_2()
     # print(tom.first_name + tom.family_name)
 
+    # "Ken Yokoyama"
     ken = Customer("Ken", "Yokoyama", 49)
     print(ken.full_name())
     print(ken.display_profile())
+    ken.display_profile_2()
     # print(ken.first_name + ken.family_name)

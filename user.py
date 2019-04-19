f8f226bda7bc7e309f280fb21f46ab7cdca830bc
Yoshiki <villager2525@gmail.com>
2019-04-19 12:04:13 +0900

    インスタンス変数の更新を学んだ｡

--- user.py
+++ user.py
@@ -1,14 +1,38 @@
 class User:
-    def __init__(self, name, age, country):
+    def __init__(self, name, age, country, hobby):
         # インスタンス変数
         self.name = name
         self.age = age
         self.country = country
+        self.hobby = hobby
+
+    def display_profile(self):
+        # display_profile は Userクラスの インスタンスメソッド
+        print(f"名前:{self.name} 国籍:{self.country} 年齢:{self.age}歳")
+
+    def hobbys(self):
+        # hobbys は Userクラスの インスタンスメソッド
+        print(f"趣味は{self.hobby}です｡")
+
+    def change_nationality(self, country_name):
+        self.country = country_name
 
 
 if __name__ == "__main__":
-    bob = User("Bob", 15, "USA")  # Userクラスをインスタンス化
+    bob = User("Bob", 15, "USA", "ゲーム")  # Userクラスをインスタンス化
+    bob.display_profile()
+    bob.hobbys()
+    bob.change_nationality("China")
+    bob.display_profile()
 
-    tom = User("Tom", 57, "USA")
+    tom = User("Tom", 57, "USA", "Netflixで映画鑑賞")
+    tom.display_profile()
+    tom.hobbys()
+    tom.change_nationality("Spain")
+    tom.display_profile()
 
-    ken = User("Ken", 49, "JPN")
+    ken = User("Ken", 49, "JPN", "プログラミング")
+    ken.display_profile()
+    ken.hobbys()
+    ken.change_nationality("Itary")
+    ken.display_profile()

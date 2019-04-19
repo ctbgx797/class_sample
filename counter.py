3741367a954b9fbd83a45142570990d0640804f9
Yoshiki <villager2525@gmail.com>
2019-04-19 15:34:07 +0900

    演習問題ファイルとTODOファイルを追加 期待値がでたからとりまOK?

---
+++ counter.py
@@ -0,0 +1,22 @@
+"""
+演習: 下記のコードが正しく動作するような Counterクラスを実装せよ
+"""
+
+
+class Counter:
+    def __init__(self, value):
+        self.value = value
+
+    def increase(self):
+        self.value += 1
+
+
+if __name__ == "__main__":
+    counter_1 = Counter(0)
+    print(counter_1.value)  # 0 が出力される
+
+    counter_1.increase()
+    print(counter_1.value)  # 1 が出力される <--- 1だけ増えてる
+
+    counter_1.increase()
+    print(counter_1.value)  # 2 が出力される <--- さらに1だけ増えている
